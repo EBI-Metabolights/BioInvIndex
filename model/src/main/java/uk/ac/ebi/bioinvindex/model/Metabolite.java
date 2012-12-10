9d655524a8c442122c0f90c1c1ee656d091d54a6
khaug <kenneth@ebi.ac.uk>
2012-12-10 15:20:11 +0000

    Motel changes to support a "cleaner" metabolite name

--- model/src/main/java/uk/ac/ebi/bioinvindex/model/Metabolite.java
+++ model/src/main/java/uk/ac/ebi/bioinvindex/model/Metabolite.java
@@ -1,23 +1,27 @@
 package uk.ac.ebi.bioinvindex.model;
 
+import org.hibernate.search.annotations.ContainedIn;
+import org.hibernate.search.annotations.Indexed;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.persistence.*;
-
-import org.hibernate.search.annotations.*;
-
 @Entity
-@Table(name = "METABOLITE")
+@Table(name = "metabolite")
 @Indexed(index = "bii")
 public class Metabolite extends Identifiable{
 
-	private Collection<MetaboliteSample> msMetSamples = new ArrayList<MetaboliteSample>();
+	private Collection<uk.ac.ebi.bioinvindex.model.MetaboliteSample> msMetSamples = new ArrayList<uk.ac.ebi.bioinvindex.model.MetaboliteSample>();
 	
-	private String description; //	NMR/MS: The small molecule's description/name.  Multiple values separated with | 
-	private String identifier; //A list of | separated possible identifiers for these small molecules. e.g. "KEGG:C000017|CHEBI:12345"
-	private String unit_id; // NMR/MS: The concatenation of the sample id plus assay id to uniquely identify that sample and run on the machine(or mass concatenated with reten tion index for unknowns)
+	private String description; //	V1.  NMR/MS: The small molecule's description/name.  Multiple values separated with |
+    //private String metabolite_identification; //	>V1. NMR/MS: The small molecule's description/name.  Multiple values separated with |
+	private String identifier; // V1. A list of | separated possible identifiers for these small molecules. e.g. "KEGG:C000017|CHEBI:12345"
+    //private String database_identifier; // >V1. A list of | separated possible identifiers for these small molecules. e.g. "KEGG:C000017|CHEBI:12345"
+    private String unit_id; // NMR/MS: The concatenation of the sample id plus assay id to uniquely identify that sample and run on the machine(or mass concatenated with reten tion index for unknowns)
 	private String chemical_formula; // NMR/MS: The chemical formula of the identified compound
+    private String smiles; // NMR/MS: The molecules structure in the simplified molecular-input line-entry system (SMILES
+    private String inchi; // NMR/MS: The standard IUPAC International Chemical Identifier (InChI) Key of the given substance.
 	private String chemical_shift; // NMR: Chemical shift
 	private String multiplicity; // NMR: Type of multiplets observed of the metabolite (e.g s, d, t, q, dd, ...)
 	private String mass_to_charge; // MS: The precursor's experimental mass to charge (m/z)
@@ -31,42 +35,42 @@ public class Metabolite extends Identifiable{
 	private String search_engine; // NMR/MS: A "|" delimited list of search engine(s) used to identify this metabolite
 	private String search_engine_score; // NMR/MS: A "|" delimited list of search engine(s) used to identify this metabolite
 	private String modifications; // MS: The small molecules modifications. The position of the modification must be given relative to the small molecules beginning. The exact semantics of this position depends on the type of small molecule identified
-	private String smallmolecule_abundance_sub; // The small moleculeÕs intensity in the respective subsample, [1-n]
-	private String smallmolecule_abundance_stdev_sub; // The standard deviation of the small moleculeÕs abundance, [1-n]
-    private String smallmolecule_abundance_std_error_sub; // The standard error of the small moleculeÕs abundance, [1-n]
+	private String smallmolecule_abundance_sub; // The small moleculeï¿½s intensity in the respective subsample, [1-n]
+	private String smallmolecule_abundance_stdev_sub; // The standard deviation of the small moleculeï¿½s abundance, [1-n]
+    private String smallmolecule_abundance_std_error_sub; // The standard error of the small moleculeï¿½s abundance, [1-n]
     private String chebiId; //Chebi Id, Initially it will be empty. It will have a value when we will curate this metabolite and therefore we, after the submission, will have a chebi id for the metabolite.
-    private String cleanedDescription;
+    private String cleanDescription; //This is the description without additional spaces and special characters
 	
 	@ContainedIn
-	private AssayGroup assayGroup;
+	private uk.ac.ebi.bioinvindex.model.AssayGroup assayGroup;
 	
 	public Metabolite(){}
-	public Metabolite(AssayGroup assayGroup, String description, String identifier){
+	public Metabolite(uk.ac.ebi.bioinvindex.model.AssayGroup assayGroup, String description, String identifier){
 		this.assayGroup = assayGroup;
 		this.description = description;
 		this.identifier = identifier;
 	}
 	
-	@ManyToOne(targetEntity = AssayGroup.class)
+	@ManyToOne(targetEntity = uk.ac.ebi.bioinvindex.model.AssayGroup.class)
 	@JoinColumn(name = "ASSAYGROUP_ID", nullable = false)
-	public AssayGroup getAssayGroup() {
+	public uk.ac.ebi.bioinvindex.model.AssayGroup getAssayGroup() {
 		return assayGroup;
 	}
-	protected void setAssayGroup(AssayGroup assayGroup) {
+
+	protected void setAssayGroup(uk.ac.ebi.bioinvindex.model.AssayGroup assayGroup) {
 		this.assayGroup = assayGroup;
 	}
 	
-	@OneToMany(targetEntity = MetaboliteSample.class,
+	@OneToMany(targetEntity = uk.ac.ebi.bioinvindex.model.MetaboliteSample.class,
 			cascade = {CascadeType.ALL})
 	@JoinColumn(name = "METABOLITE_ID", nullable = true)
-	public Collection<MetaboliteSample> getMetaboliteSamples() {
+	public Collection<uk.ac.ebi.bioinvindex.model.MetaboliteSample> getMetaboliteSamples() {
 		return msMetSamples;
 	}
 
-	public void setMetaboliteSamples(Collection<MetaboliteSample> msMetaboliteSamples) {
+	public void setMetaboliteSamples(Collection<uk.ac.ebi.bioinvindex.model.MetaboliteSample> msMetaboliteSamples) {
 		this.msMetSamples = msMetaboliteSamples;
 	}
-
 
 	public String getDescription() {
 		return description;
@@ -75,31 +79,83 @@ public class Metabolite extends Identifiable{
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	/**
-	 * TODO: Have cleaned description for story Duplicated metabolites and about half? of them do not match any study.
-	 * https://www.pivotaltracker.com/projects/620203#!/stories/35586843
-	 * Cleanned description is a way of normalizing descriptions that differ in spaces like:
-	 * "(2-methoxyethoxy)propanoic acid "  --> no space between ) and propanoic
-	 * "(2-methoxyethoxy) propanoic acid" --> with space and no space at the end
-	 * "(2-methoxyethoxy) propanoic acid " --> with space and no space at the end
-	 * @return
-	 */
-	public String getCleanedDescription() {
-		
-		if (cleanedDescription == null)
-		{
-			cleanedDescription = description.replaceAll(" ", "");
-		}
-		return cleanedDescription;
-	}
 
-	public String getIdentifier() {
+    @Transient()
+    public String getMetabolite_identification() {
+        return description;          //Will return the value in the description field
+    }
+
+    public void setMetabolite_identification(String metabolite_identification) {
+        this.description = metabolite_identification;
+    }
+
+    /**
+     * TODO: Have cleaned description for story Duplicated metabolites and about half? of them do not match any study.
+     * https://www.pivotaltracker.com/projects/620203#!/stories/35586843
+     * Cleanned description is a way of normalizing descriptions that differ in spaces like:
+     * "(2-methoxyethoxy)propanoic acid "  --> no space between ) and propanoic
+     * "(2-methoxyethoxy) propanoic acid" --> with space and no space at the end
+     * "(2-methoxyethoxy) propanoic acid " --> with space and no space at the end
+     */
+    public void setCleanDescription(String cleanDescription) {
+
+        this.cleanDescription = cleanDescription;
+
+        if (cleanDescription == null)
+            this.cleanDescription = getDescription();
+
+        try {
+
+            if (this.cleanDescription != null)
+            {
+                this.cleanDescription = this.cleanDescription.trim();  //Heading and trailing spaces removed
+
+                //TODO,change replaceAlls with proper regex
+                this.cleanDescription = this.cleanDescription.replaceAll("  "," ");    //Remove two spaces
+
+                this.cleanDescription = this.cleanDescription.replaceAll(" \\) ",")"); //Remove space after )
+                this.cleanDescription = this.cleanDescription.replaceAll(" \\)",")");  //Remove space before )
+
+                this.cleanDescription = this.cleanDescription.replaceAll("] ","]");    //Remove space after ]
+                this.cleanDescription = this.cleanDescription.replaceAll(" ]","]");    //Remove space after ]
+
+                this.cleanDescription = this.cleanDescription.replaceAll(" /","/");    //Remove space before /
+                this.cleanDescription = this.cleanDescription.replaceAll("/ ","/");    //Remove space after /
+                this.cleanDescription = this.cleanDescription.replaceAll("-/","/");    //Remove - before /
+                this.cleanDescription = this.cleanDescription.replaceAll("/-","/");    //Remove - after /
+                //this.cleanDescription = this.cleanDescription.replaceAll("/"," ");     //Replace all / with a space.  //TODO, should be parsed as two entries
+                //this.cleanDescription = this.cleanDescription.replaceAll(" or "," ");  //Replace all " or " with a space.  //TODO, should be parsed as two entries
+                //this.cleanDescription = this.cleanDescription.replaceAll("\\?","");    //Remove question marks     //TODO, should feed back to submitter
+                this.cleanDescription = this.cleanDescription.trim();  // no trailing spaces
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getCleanDescription() {
+        return cleanDescription;
+    }
+
+    public String getIdentifier() {
 		return identifier;
 	}
 
 	public void setIdentifier(String identifier) {
 		this.identifier = identifier;
 	}
+
+    @Transient
+    public String getDatabase_identifier() {
+        return identifier;   // Will return the value in the identifier field
+    }
+
+    public void setDatabase_identifier(String database_identifier) {
+        this.identifier = database_identifier;
+    }
+
 	public String getUnit_id() {
 		return unit_id;
 	}
@@ -116,7 +172,23 @@ public class Metabolite extends Identifiable{
 		this.chemical_formula = chemical_formula;
 	}
 
-	public String getChemical_shift() {
+    public String getSmiles() {
+        return smiles;
+    }
+
+    public void setSmiles(String smiles) {
+        this.smiles = smiles;
+    }
+
+    public String getInchi() {
+        return inchi;
+    }
+
+    public void setInchi(String inchi) {
+        this.inchi = inchi;
+    }
+
+    public String getChemical_shift() {
 		return chemical_shift;
 	}
 
@@ -227,6 +299,7 @@ public class Metabolite extends Identifiable{
 	public void setSmallmolecule_abundance_sub(String smallmolecule_abundance_sub) {
 		this.smallmolecule_abundance_sub = smallmolecule_abundance_sub;
 	}
+
 	@Column (name="SMA_STDEV_SUB")
 	public String getSmallmolecule_abundance_stdev_sub() {
 		return smallmolecule_abundance_stdev_sub;
@@ -236,7 +309,8 @@ public class Metabolite extends Identifiable{
 			String smallmolecule_abundance_stdev_sub) {
 		this.smallmolecule_abundance_stdev_sub = smallmolecule_abundance_stdev_sub;
 	}
-	@Column (name="SMA_STD_ERR_SUB")
+
+	@Column(name="SMA_STD_ERR_SUB")
 	public String getSmallmolecule_abundance_std_error_sub() {
 		return smallmolecule_abundance_std_error_sub;
 	}
@@ -245,6 +319,7 @@ public class Metabolite extends Identifiable{
 			String smallmolecule_abundance_std_error_sub) {
 		this.smallmolecule_abundance_std_error_sub = smallmolecule_abundance_std_error_sub;
 	}
+
 	public String getChebiId() {
 		return chebiId;
 	}
--- model/src/main/java/uk/ac/ebi/bioinvindex/search/hibernatesearch/bridge/AssayGroupBridge.java
+++ model/src/main/java/uk/ac/ebi/bioinvindex/search/hibernatesearch/bridge/AssayGroupBridge.java
@@ -4,11 +4,8 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.hibernate.search.bridge.FieldBridge;
 import org.hibernate.search.bridge.LuceneOptions;
-
 import uk.ac.ebi.bioinvindex.model.AssayGroup;
 import uk.ac.ebi.bioinvindex.model.Metabolite;
-import uk.ac.ebi.bioinvindex.model.security.Person;
-import uk.ac.ebi.bioinvindex.model.security.User;
 
 import java.util.Collection;
 
@@ -43,18 +40,39 @@ public class AssayGroupBridge extends IndexFieldDelimiters implements FieldBridg
             
         }
     }
+
     private void indexMetabolites(AssayGroup ag, Document doc, LuceneOptions luceneOptions){
     	
     	// Go through the list of metabolites
     	for (Metabolite met: ag.getMetabolites()){
-    		
-    		String representation = met.getDescription() + "~" + met.getIdentifier();
-    		System.out.println("Indexing Metabolite: " + representation);
-    		
-    		Field fvField =
-                new Field("Metabolite", representation, luceneOptions.getStore(), luceneOptions.getIndex());
 
-    		doc.add(fvField);
+            String metabolite = met.getCleanDescription();
+    		//String representation = met.getDescription() + "~" + met.getIdentifier();
+            String representation = metabolite + "~" + met.getIdentifier();
+
+            //Ken 20121203, should not be required anymore as this is "cleaned" in the Metabolite class
+            //Skip if submitter has listed more than one metabolite per line ("/", "?", " or ")
+            /*if (!metabolite.contains("?")
+                    && !metabolite.contains("/")
+                    && !metabolite.contains("|")
+                    && !metabolite.contains("+")
+                    && !metabolite.contains(", ")
+                    && !metabolite.contains("_")
+                    && !metabolite.contains("- ") && !metabolite.contains(" -")
+                    && !metabolite.contains("unknown") && !metabolite.contains("unidentified") && !metabolite.contains("unassiged") && !metabolite.contains("unk-")
+                    && !metabolite.contains("#")
+                    && !metabolite.contains(" or ") && !metabolite.contains(" and ")
+                    ){
+              */
+
+                System.out.println("Indexing Metabolite: " + representation);
+
+
+                Field fvField =
+                    new Field("Metabolite", representation, luceneOptions.getStore(), luceneOptions.getIndex());
+
+                doc.add(fvField);
+            //}
     
     	}
     	
